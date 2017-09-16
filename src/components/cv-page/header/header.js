ab19e732e1ed360ac21a526a33b82200ce7cfcca
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-09-16 10:45:56 +0200

    first commit

---
+++ src/components/cv-page/header/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <div  className="header__pic"></div>
+        <div  className="header__about-me"></div>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/cv-page/main/main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class CVPage extends Component {
+  render() {
+    return (
+      <div className="main">
+      </div>
+    );
+  }
+}
+
+export default CVPage;
