507091e0bd48f51cd302a00149dc9faf0030f5a7
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-09-18 18:33:12 +0200

    adding section text style5

---
+++ src/components/cv-page/main/education/__education__education-progress-bar/education__education-progress-bar.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+// import BarAndDates from './__education__education-progress-bar__bar-and-dates/education__education-progress-bar__bar-and-dates.js';
+
+
+class EducationProgressBar extends Component {
+  render() {
+    return (
+    <div className="education__education-progress-bar">
+
+      <ul className="education__education-progress-bar__el">
+        <li className="education__education-progress-bar__el__item">
+          <p className="education__education-progress-bar__el__item__text">Liceum handlowe w Toruniu</p>
+        </li>
+        <li className="education__education-progress-bar__el__item education__education-progress-bar__el__item_color-background">
+          <strong className="education__education-progress-bar__el__item__text education__education-progress-bar__el__item__text_color-background ">NKJO w Toruniu</strong>
+        </li>
+        <li className="education__education-progress-bar__el__item">
+          <p className="education__education-progress-bar__el__item__text">UMK, Filologia angielska, lingwistyka</p>
+        </li>
+        <li className="education__education-progress-bar__el__item education__education-progress-bar__el__item_color-background">
+          <strong className="education__education-progress-bar__el__item__text education__education-progress-bar__el__item__text_color-background">Coders Lab</strong>
+        </li>
+      </ul>
+
+      <ul className="education__education-progress-bar__el">
+        <li className="education__education-progress-bar__el__item education__education-progress-bar__el__item_line education__education-progress-bar__el__item_line_0">
+          <p className="education__education-progress-bar__el__item__text education__education-progress-bar__el__item__text__circle-0">Czerwiec 2005</p>
+        </li>
+        <li className="education__education-progress-bar__el__item education__education-progress-bar__el__item_line education__education-progress-bar__el__item_line_1">
+          <p className="education__education-progress-bar__el__item__text  education__education-progress-bar__el__item__text__circle-1">Czerwiec 2009</p>
+        </li>
+        <li className="education__education-progress-bar__el__item education__education-progress-bar__el__item_line education__education-progress-bar__el__item_line_2">
+          <p className="education__education-progress-bar__el__item__text education__education-progress-bar__el__item__text__circle-2">Lipiec 2015</p>
+        </li>
+        <li className="education__education-progress-bar__el__item education__education-progress-bar__el__item_line education__education-progress-bar__el__item_line_last">
+          <p className="education__education-progress-bar__el__item__text  education__education-progress-bar__el__item__text__circle-3">Marzec 2017</p>
+        </li>
+      </ul>
+
+      <ul className="education__education-progress-bar__el education__education-progress-bar__el_last">
+        <li className="education__education-progress-bar__el__item education__education-progress-bar__el__item_color-background">
+          <p className="education__education-progress-bar__el__item__text education__education-progress-bar__el__item__text_color-background"><span className="education__education-progress-bar__el__item__text_color-background_strong">Technik</span> Spec. handel</p>
+        </li>
+        <li className="education__education-progress-bar__el__item">
+          <p className="education__education-progress-bar__el__item__text">Nauczyciel jęz. angielskiego</p>
+        </li>
+        <li className="education__education-progress-bar__el__item education__education-progress-bar__el__item_color-background">
+          <p className="education__education-progress-bar__el__item__text_color-background"><span className="education__education-progress-bar__el__item__text_color-background_strong">Magister</span> jęz. angielski</p>
+        </li>
+        <li className="education__education-progress-bar__el__item">
+          <p className="education__education-progress-bar__el__item__text">Dyplom Front end with react</p>
+        </li>
+      </ul>
+
+    </div>
+    );
+  }
+}
+
+export default EducationProgressBar;
--- src/components/cv-page/main/education/education.js
+++ src/components/cv-page/main/education/education.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import EducationProgressBar from './__education__education-progress-bar/education__education-progress-bar';
 
 
 export class Education extends Component {
@@ -8,8 +9,14 @@ export class Education extends Component {
         <div className="education__el education__text">
           <p className="education__el education__text_break">edukacja~</p>
         </div>
-        <div className="education__el education__content">
-          education__content
+        <div className="education__el ">
+          <div className="education__content">
+            <div className="education__section-name">
+              <div className="education__section-name__icon_education"></div>
+              <p className="education__section-name__text_education">Moje szkoły</p>
+            </div>
+            <EducationProgressBar></EducationProgressBar>
+          </div>
         </div>
       </div>
     );
