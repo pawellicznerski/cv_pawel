000ef8e4df57e5cf835a519637e156b6fa232002
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-09-19 19:48:05 +0200

    finished experience section

--- src/components/cv-page/main/interests/interests.js
+++ src/components/cv-page/main/interests/interests.js
@@ -5,12 +5,30 @@ export class Interests extends Component {
   render() {
     return (
       <div className="interests">
+
         <div className="interests__el interests__text">
           <p className="interests__el interests__text_break">zainteresowania~</p>
         </div>
-        <div className="interests__el interests__content">
-          zainteresowania~
+
+        <div className="interests__el">
+          <div className="interests__content">
+
+            <div className=" interests__section-name">
+              <div className=" interests__section-name__icon_interests"></div>
+              <p className=" interests__section-name__text_interests">Zainteresowania</p>
+            </div>
+
+            <div className="interests__cirles">
+              <div className="interests__cirles_item interests__cirles_item_dark-main">Front-end
+                <div className="interests__cirles_item interests__cirles_item_dark-main_lightest">Ultramaratony rowerowe</div>
+                <div className="interests__cirles_item interests__cirles_item_dark-main_light">Muzyka</div>
+                <div className="interests__cirles_item interests__cirles_item_dark-main_darkest">Kino</div>
+              </div>
+            </div>
+
+          </div>
         </div>
+
       </div>
     );
   }
