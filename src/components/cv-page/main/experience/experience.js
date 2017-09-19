b2968127a8ded45aa0b25515f532fde0770a223e
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-09-19 18:28:49 +0200

    finished experience section

--- src/components/cv-page/main/experience/experience.js
+++ src/components/cv-page/main/experience/experience.js
@@ -1,67 +1,54 @@
 import React, { Component } from 'react';
 
+class Job extends Component {
+  addClasses(basicClass){
+    return `${basicClass} ${"experience__content__section__el__text_arrow_"+this.props.arrow}`
+  }
+  render() {
+    return (
+      <div className="experience__content__section__el">
+        <p className="experience__content__section__el__text_title">{this.props.company}</p>
+        <p className="experience__content__section__el__text_main">{this.props.duties}</p>
+        <span className={this.addClasses("experience__content__section__el__text_arrow")}></span>
+        <p className="experience__content__section__el__text_date">{this.props.year}</p>
+      </div>
+    );
+  }
+}
 
 export class Experience extends Component {
   render() {
     return (
       <div className="experience">
+
         <div className="experience__el experience__text">
           <p className="experience__el experience__text_break">doświadczenie~</p>
         </div>
 
-        <div className="experience__el  experience2">
-
-          <div className="experience2__section-name">
-            <div className="experience2__section-name__icon_experience2"></div>
-            <p className="experience2__section-name__text_experience2">Moja praca</p>
+        <div className="experience__el  experience__content">
+          <div className="experience__content__section-name">
+            <div className="experience__content__section-name__icon"></div>
+            <p className="experience__content__section-name__text">Moja praca</p>
           </div>
 
-          <div className="experience2__chart">
-            <div className="experience2__section experience2__section_left">
-
-              <div className="experience2__section__el">
-                <p className="experience2__section__el__text_title">Manpower/Sharp Polska</p>
-                <p className="experience2__section__el__text_main">Kontroler jakości: kontrolowanie prawidłowości procesu produkcji i jakości komponentów do telewizorów.</p>
-                <span className="experience2__section__el__text_arrow experience2__section__el__text_arrow_left"></span>
-                <div className="experience2__section__el__text_date">
-                  <p className="experience2__section__el__text_date_year">2011-2012</p>
-                </div>
-              </div>
-
-              <div className="experience2__section__el experience2__section__el_left">
-                <p className="experience2__section__el__text_title">Esada Błażej Martuszewski</p>
-                <p className="experience2__section__el__text_main">Logistyk/operator vendingowy: zamawianie towaru, organizacja pracy pracowników.</p>
-                <span className="experience2__section__el__text_arrow experience2__section__el__text_arrow_left"></span>
-                <div className="experience2__section__el__text_date">
-                  <p className="experience2__section__el__text_date_year">2013-2016</p>
-                </div>
-              </div>
-
+          <div className="experience__content__chart">
+            <div className="experience__content__section experience__content__section_left">
+              <Job company="Manpower/Sharp Polska" year="2011-2012" arrow="left"
+                duties="Kontroler jakości: kontrolowanie prawidłowości procesu produkcji i jakości komponentów do telewizorów."
+                />
+              <Job company="Esada Błażej Martuszewski" year="2013-2016" arrow="left"
+                duties="Logistyk/operator vendingowy: zamawianie towaru, organizacja pracy pracowników."
+                />
             </div>
-
-            <div className="experience2__section experience2__section_right">
-              <div className="experience2__section__el experience2__section__el_right">
-                <p className="experience2__section__el__text_title">Własna firma P.H. Paweł Licznerski</p>
-                <p className="experience2__section__el__text_main">Firma vendingowa jednoosobowa, zajmująca się sprzedażą kanapek z automatów.</p>
-                <span className="experience2__section__el__text_arrow experience2__section__el__text_arrow_right"></span>
-                <div className="experience2__section__el__text_date">
-                  <p className="experience2__section__el__text_date_year">2012-2013</p>
-                </div>
-              </div>
-
-              <div className="experience2__section__el experience2__section__el_right">
-                <p className="experience2__section__el__text_title">Limito S.A.</p>
-                <p className="experience2__section__el__text_main">Spec. ds. zakupów: dokonywanie zakupów opakowao do łososia, negocjacja cen.</p>
-                <span className="experience2__section__el__text_arrow experience2__section__el__text_arrow_right"></span>
-                <div className="experience2__section__el__text_date">
-                  <p className="experience2__section__el__text_date_year">2016-2017</p>
-                </div>
-              </div>
-
+            <div className="experience__content__section experience__content__section_right">
+              <Job company="Własna firma P.H. Paweł Licznerski" year="2012-2013" arrow="right"
+                duties="Firma vendingowa jednoosobowa, zajmująca się sprzedażą kanapek z automatów."
+                />
+              <Job company="Limito S.A." year="2016-2017" arrow="right"
+                duties="Spec. ds. zakupów: dokonywanie zakupów opakowao do łososia, negocjacja cen."
+                />
             </div>
           </div>
-
-
         </div>
 
       </div>
