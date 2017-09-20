fe2be5b0a52aefd05782625fad9134c77440bf84
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-09-20 18:03:43 +0200

    adding png's to portfolio

--- src/components/cv-page/main/portfolio/portfolio.js
+++ src/components/cv-page/main/portfolio/portfolio.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react';
 import { SectionElHeader } from './../__section__el_header/section__el_header';
 
 class Project extends Component{
-  addIconClass(){
-    return `portfolio__projects__project__main__icon ${"portfolio__projects__project__main__icon_"+this.props.name}`
+
+  addMainClass(basicClass){
+    return `${basicClass} ${"portfolio__projects__project__main_"+this.props.color}`
   }
-  addMainClass(){
-    return `portfolio__projects__project__main ${"portfolio__projects__project__main_"+this.props.color}`
+  addIconClass(basicClass){
+    return `${basicClass} ${"portfolio__projects__project__main__icon_"+this.props.name}`
   }
   render(){
     return(
       <div className="portfolio__projects__project">
 
         <h2 className="portfolio__projects__project__no">{this.props.no}</h2>
-        <div className={this.addMainClass()}>
-          <div className={this.addIconClass()}>
+        <div className={this.addMainClass('portfolio__projects__project__main')}>
+          <div className={this.addIconClass('portfolio__projects__project__main__icon')}>
+            {this.props.text}
           </div>
           <a className="portfolio__projects__project__main__link" href={`https://pawellicznerski.github.io/${this.props.demo}`}>demo</a>
           <a className="portfolio__projects__project__main__link" href={`https://github.com/pawellicznerski/${this.props.code}`}>kod</a>
@@ -35,11 +37,11 @@ export class Portfolio extends Component {
           <SectionElHeader text="portfolio" icon="portfolio" colorChange="true"/>
 
           <div className="portfolio__projects">
-            <Project no="5" name="ultramarathon" demo="Ultramarathon_Training_App/" code="Ultramarathon_Training_App" color="olive"/>
-            <Project no="4" name="ultramarathon" demo="Ultramarathon_Training_App/" code="Ultramarathon_Training_App" color="yellow"/>
-            <Project no="3" name="ultramarathon" demo="Ultramarathon_Training_App/" code="Ultramarathon_Training_App" color="orange"/>
-            <Project no="2" name="cvproject" demo="CV_project/" code="CV_project" color="grey"/>
-            <Project no="1" name="ultramarathon" demo="Ultramarathon_Training_App/" code="Ultramarathon_Training_App" color="green"/>
+            <Project no="5" name="pawellicznerski" text='CV Paweł Licznerski' demo="Ultramarathon_Training_App/" code="Ultramarathon_Training_App" color="olive"/>
+            <Project no="4" name="findyourfilm" text='' demo="Ultramarathon_Training_App/" code="Ultramarathon_Training_App" color="yellow"/>
+            <Project no="3" name="" text='Your todo list...' demo="Ultramarathon_Training_App/" code="Ultramarathon_Training_App" color="orange"/>
+            <Project no="2" name="cvproject" text='' demo="CV_project/" code="CV_project" color="grey"/>
+            <Project no="1" name="ultramarathon" text='' demo="Ultramarathon_Training_App/" code="Ultramarathon_Training_App" color="green"/>
           </div>
 
         </div>
