e3f7f1ebf59a81765ea2baf81ef4436a2230dfc8
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-09-28 21:33:06 +0200

    adding fliping footer

--- src/components/cv-page/footer/footer.js
+++ src/components/cv-page/footer/footer.js
@@ -5,11 +5,16 @@ class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        <p className="footer__section footer__section_left">
-          &copy; Paweł Licznerski
-        </p>
-        <p className="footer__section footer__section_right">
-          Wyrażam zgodę na przetwarzanie moich danych osobowych dla potrzeb niezbędnych do realizacji procesu rekrutacji (zgodnie z Ustawą z dnia 29.08.1997 roku o Ochronie Danych Osobowych; tekst jednolity: Dz. U. 2016 r. poz. 922). 
+        <div className="footer__section footer__section__left">
+          <div className="footer__section__left__word"><span className="footer__section__left__word_syllable">&copy;</span></div>
+          <div className="footer__section__left__word"><span className="footer__section__left__word_syllable">Pa</span></div>
+          <div className="footer__section__left__word"><span className="footer__section__left__word_syllable">weł</span></div>
+          <div className="footer__section__left__word"><span className="footer__section__left__word_syllable">Li</span></div>
+          <div className="footer__section__left__word"><span className="footer__section__left__word_syllable">czner</span></div>
+          <div className="footer__section__left__word"><span className="footer__section__left__word_syllable">ski</span></div>
+        </div>
+        <p className="footer__section footer__section__right">
+          Wyrażam zgodę na przetwarzanie moich danych osobowych dla potrzeb niezbędnych do realizacji procesu rekrutacji (zgodnie z Ustawą z dnia 29.08.1997 roku o Ochronie Danych Osobowych; tekst jednolity: Dz. U. 2016 r. poz. 922).
         </p>
       </div>
     );
