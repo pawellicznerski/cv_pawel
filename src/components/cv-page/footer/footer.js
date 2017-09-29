c03b3413e4c22e17a293db610c43f63ec510c7ad
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-09-29 18:25:55 +0200

    adding fliping footer

--- src/components/cv-page/footer/footer.js
+++ src/components/cv-page/footer/footer.js
@@ -1,5 +1,25 @@
 import React, { Component } from 'react';
 
+componentDidMount(){
+  window.addEventListener('scroll', this.handleScroll);
+}
+
+componentWillUnmount(){
+  window.removeEventListener('scroll', this.handleScroll);
+}
+
+handleScroll(e){
+  e.preventDefault();
+      if (window.scrollY > window.innerHeight-67) {
+        this.setState({
+          topScrollBtn:true,
+          });
+      } else if(window.scrollY < window.innerHeight-67){
+        this.setState({
+          topScrollBtn:false,
+          });
+      }
+}
 
 class Footer extends Component {
   render() {
@@ -7,11 +27,21 @@ class Footer extends Component {
       <div className="footer">
         <div className="footer__section footer__section__left">
           <div className="footer__section__left__word"><span title="&copy;" className="footer__section__left__word_syllable">&copy;</span></div>
-          <div className="footer__section__left__word"><span title="Pa" className="footer__section__left__word_syllable">Pa</span></div>
-          <div className="footer__section__left__word"><span title="weł" className="footer__section__left__word_syllable">weł</span></div>
-          <div className="footer__section__left__word"><span title="Li" className="footer__section__left__word_syllable">Li</span></div>
-          <div className="footer__section__left__word"><span title="czner" className="footer__section__left__word_syllable">czner</span></div>
-          <div className="footer__section__left__word"><span title="ski" className="footer__section__left__word_syllable">ski</span></div>
+          <div className="footer__section__left__word"><span title="P" className="footer__section__left__word_syllable"> P</span></div>
+          <div className="footer__section__left__word"><span title="a" className="footer__section__left__word_syllable">a</span></div>
+          <div className="footer__section__left__word"><span title="w" className="footer__section__left__word_syllable"> w</span></div>
+          <div className="footer__section__left__word"><span title="e" className="footer__section__left__word_syllable">e</span></div>
+          <div className="footer__section__left__word"><span title="ł" className="footer__section__left__word_syllable">ł</span></div>
+          <div className="footer__section__left__word"><span title="L" className="footer__section__left__word_syllable">L</span></div>
+          <div className="footer__section__left__word"><span title="i" className="footer__section__left__word_syllable">i</span></div>
+          <div className="footer__section__left__word"><span title="c" className="footer__section__left__word_syllable">c</span></div>
+          <div className="footer__section__left__word"><span title="z" className="footer__section__left__word_syllable">z</span></div>
+          <div className="footer__section__left__word"><span title="n" className="footer__section__left__word_syllable">n</span></div>
+          <div className="footer__section__left__word"><span title="e" className="footer__section__left__word_syllable">e</span></div>
+          <div className="footer__section__left__word"><span title="r" className="footer__section__left__word_syllable">r</span></div>
+          <div className="footer__section__left__word"><span title="s" className="footer__section__left__word_syllable">s</span></div>
+          <div className="footer__section__left__word"><span title="k" className="footer__section__left__word_syllable">k</span></div>
+          <div className="footer__section__left__word"><span title="i" className="footer__section__left__word_syllable">i</span></div>
         </div>
         <p className="footer__section footer__section__right">
           Wyrażam zgodę na przetwarzanie moich danych osobowych dla potrzeb niezbędnych do realizacji procesu rekrutacji (zgodnie z Ustawą z dnia 29.08.1997 roku o Ochronie Danych Osobowych; tekst jednolity: Dz. U. 2016 r. poz. 922).
