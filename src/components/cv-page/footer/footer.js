a6d1af79a4ea2b17a845d4bbb10f378b718d5696
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-09-29 21:12:12 +0200

    adding fliping footer

--- src/components/cv-page/footer/footer.js
+++ src/components/cv-page/footer/footer.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 
+class Footer extends Component {
+constructor(props){
+  super(props);
+  this.state={
+    isAnimated:false,
+  }
+  this.handleScroll=this.handleScroll.bind(this);
+}
+
 componentDidMount(){
   window.addEventListener('scroll', this.handleScroll);
 }
@@ -10,38 +19,45 @@ componentWillUnmount(){
 
 handleScroll(e){
   e.preventDefault();
-      if (window.scrollY > window.innerHeight-67) {
-        this.setState({
-          topScrollBtn:true,
-          });
-      } else if(window.scrollY < window.innerHeight-67){
-        this.setState({
-          topScrollBtn:false,
-          });
-      }
-}
+    if (window.scrollY > window.innerHeight-67) {
+      this.setState({
+        isAnimated:true,
+        });
+        console.log(this.state.isAnimated);
+    }
+    // else if(window.scrollY < window.innerHeight-67){
+    //   this.setState({
+    //     isAnimated:false,
+    //     });
+    // }
+  }
 
-class Footer extends Component {
   render() {
     return (
       <div className="footer">
         <div className="footer__section footer__section__left">
-          <div className="footer__section__left__word"><span title="&copy;" className="footer__section__left__word_syllable">&copy;</span></div>
-          <div className="footer__section__left__word"><span title="P" className="footer__section__left__word_syllable"> P</span></div>
-          <div className="footer__section__left__word"><span title="a" className="footer__section__left__word_syllable">a</span></div>
-          <div className="footer__section__left__word"><span title="w" className="footer__section__left__word_syllable"> w</span></div>
-          <div className="footer__section__left__word"><span title="e" className="footer__section__left__word_syllable">e</span></div>
-          <div className="footer__section__left__word"><span title="ł" className="footer__section__left__word_syllable">ł</span></div>
-          <div className="footer__section__left__word"><span title="L" className="footer__section__left__word_syllable">L</span></div>
-          <div className="footer__section__left__word"><span title="i" className="footer__section__left__word_syllable">i</span></div>
-          <div className="footer__section__left__word"><span title="c" className="footer__section__left__word_syllable">c</span></div>
-          <div className="footer__section__left__word"><span title="z" className="footer__section__left__word_syllable">z</span></div>
-          <div className="footer__section__left__word"><span title="n" className="footer__section__left__word_syllable">n</span></div>
-          <div className="footer__section__left__word"><span title="e" className="footer__section__left__word_syllable">e</span></div>
-          <div className="footer__section__left__word"><span title="r" className="footer__section__left__word_syllable">r</span></div>
-          <div className="footer__section__left__word"><span title="s" className="footer__section__left__word_syllable">s</span></div>
-          <div className="footer__section__left__word"><span title="k" className="footer__section__left__word_syllable">k</span></div>
-          <div className="footer__section__left__word"><span title="i" className="footer__section__left__word_syllable">i</span></div>
+          <div className="footer__section__left__word">
+            <span
+              title="&copy;"
+              className={this.state.isAnimated?"footer__section__left__word_letter":"footer__section__left__word_letter_1"}
+            >&copy;</span>
+          </div>
+          <div className="footer__section__left__word">
+            <span title="P" className="footer__section__left__word_letter">P</span></div>
+          <div className="footer__section__left__word"><span title="a" className="footer__section__left__word_letter">a</span></div>
+          <div className="footer__section__left__word"><span title="w" className="footer__section__left__word_letter">w</span></div>
+          <div className="footer__section__left__word"><span title="e" className="footer__section__left__word_letter">e</span></div>
+          <div className="footer__section__left__word"><span title="ł" className="footer__section__left__word_letter">ł</span></div>
+          <div className="footer__section__left__word"><span title="L" className="footer__section__left__word_letter">L</span></div>
+          <div className="footer__section__left__word"><span title="i" className="footer__section__left__word_letter">i</span></div>
+          <div className="footer__section__left__word"><span title="c" className="footer__section__left__word_letter">c</span></div>
+          <div className="footer__section__left__word"><span title="z" className="footer__section__left__word_letter">z</span></div>
+          <div className="footer__section__left__word"><span title="n" className="footer__section__left__word_letter">n</span></div>
+          <div className="footer__section__left__word"><span title="e" className="footer__section__left__word_letter">e</span></div>
+          <div className="footer__section__left__word"><span title="r" className="footer__section__left__word_letter">r</span></div>
+          <div className="footer__section__left__word"><span title="s" className="footer__section__left__word_letter">s</span></div>
+          <div className="footer__section__left__word"><span title="k" className="footer__section__left__word_letter">k</span></div>
+          <div className="footer__section__left__word"><span title="i" className="footer__section__left__word_letter">i</span></div>
         </div>
         <p className="footer__section footer__section__right">
           Wyrażam zgodę na przetwarzanie moich danych osobowych dla potrzeb niezbędnych do realizacji procesu rekrutacji (zgodnie z Ustawą z dnia 29.08.1997 roku o Ochronie Danych Osobowych; tekst jednolity: Dz. U. 2016 r. poz. 922).
